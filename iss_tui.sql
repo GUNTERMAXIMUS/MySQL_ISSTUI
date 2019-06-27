d21cb7ca4712e20252ff5265ba66be2f3600b8c8
Benny Serra <benjaserrau@gmail.com>
2019-06-27 16:26:35 -0400

    Update iss_tui.sql

--- iss_tui.sql
+++ iss_tui.sql
@@ -1,72 +1,112 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 27-06-2019 a las 01:20:47
--- Versión del servidor: 10.3.15-MariaDB
--- Versión de PHP: 7.3.6
+import csv, json, os, mysql.connector
+from mysql.connector import errorcode
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+#constantes
+HOST="host"
+USER="user"
+PASSWD="password"
+DB="database"
+PORT="port"
+inicio=0
+
+#dictionary vacío
+configuracion={}
+
+while inicio==0:
+	inicio+=1
+	#MENU INICIO: argumentos de conexión definidos por el usuario
+	print("\n/////////////////////////////////////////////////////////")
+	print("\t>>>BIENVENIDO A MYSQL-CONNECTOR-PYTHON<<<")
+	print("////////////////////////////////////////////////////////")
+	host=input("\n\t>HOST: ingrese host (ej.localhost): ")
+	user=input("\t>USER: ingrese usuario (ej. root): ")
+	password=input("\t>PASSWORD: ingrese contraseña (puede estar vacío): ")
+	database=input("\t>DATABASE: ingrese base de datos: ")
+	port=input("\t>PORT: ingrese puerto (default: 3306): ")
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+	#los argumentos de conexión definidos en un diccionario [con keys:values] para su mejor manipulación
+	#otra opción --> configuracion=mysql.connector.connect(option_files='/etc/mysql/connectors.cnf')
+	configuracion[HOST]=host
+	configuracion[USER]=user
+	configuracion[PASSWD]=password
+	configuracion[DB]=database
+	configuracion[PORT]=port
+	configuracion[AUTO]=auto
 
---
--- Base de datos: `iss_tui`
---
+	#conectando a la DB
+	try:
+		conexion_db=mysql.connector.connect(**configuracion)
+		print("\n\t>>>CONECTADO EXITOSAMENTE A LA DATABASE:",database)
+		print("\t>>>Bienvenido",user)
 
--- --------------------------------------------------------
+		#cursor (nos permite ejecutar y usar los comandos SQL en la sesión actual)
+		#nos da la ventaja de tener multiples y separados ambientes de trabajo a traves de la misma conexión a la base de datos.
+		cursor=conexion_db.cursor()
 
---
--- Estructura de tabla para la tabla `registros`
---
+		while inicio==1:
+			inicio+=1
+			archivo=input("\n\tIngrese nombre del archivo.csv o path (ej. C:/users/user/desktop/carpeta/archivo.csv): ")
+			if os.path.exists(archivo):
+				with open(archivo,"r") as csv_file:
+					csv_data=csv.reader(csv_file,delimiter=" ")
+					print("\n\t>>LEYENDO",archivo) 
+					for row in csv_data:
+						print("\t",row) #imprime una lista de todos los valores por cada linea 
+						"""
+						cursor.execute("INSERT INTO registros (id,clave_profesor, nombre, apellido, fecha, hora_de_ingreso, hora_de_salida, sala)" 
+							"VALUES(%s, %s, %s, %s, %s, %s, %s, %s)",row)
+						print("datos csv insertados a la db")
+						"""
+			else:
+				print("\n\t>>El archivo no existe")
+				inicio=1
 
-CREATE TABLE `registros` (
-  `id` int(11) NOT NULL,
-  `clave_profesor` int(4) DEFAULT NULL,
-  `nombre` varchar(15) DEFAULT 'NULL',
-  `apellido` varchar(15) DEFAULT 'NULL',
-  `fecha` date DEFAULT NULL,
-  `hora_de_ingreso` time DEFAULT NULL,
-  `hora_de_salida` time DEFAULT NULL,
-  `sala` int(3) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+		#ejecutar la query 
+		cursor.execute("SELECT * FROM registros") #(execute: prepara la sentencia)
+		rows=cursor.fetchall()#(presenta el resultado | FETCH ALL)
+		for i in rows:
+			print("\n\t>>REGISTROS DATABASE:")
+			print("\t",i)
 
---
--- Volcado de datos para la tabla `registros`
---
+		#los datos (ingresados) se perpetrar(commit) a la base de datos
+		conexion_db.commit()
 
-INSERT INTO `registros` (`id`, `clave_profesor`, `nombre`, `apellido`, `fecha`, `hora_de_ingreso`, `hora_de_salida`, `sala`) VALUES
-(1, 1111, 'Oscar', 'Aravena', '2019-06-12', '08:30:00', '11:00:00', 301);
+	#para error de conexión
+	except mysql.connector.Error as err:
+	  if err.errno==errorcode.ER_ACCESS_DENIED_ERROR:
+	    print("\n\t>>USUARIO o PASSWORD incorrectos")
+	    inicio=0
+	  elif err.errno==errorcode.ER_BAD_DB_ERROR:
+	    print("\n\t>>La DATABASE no existe")
+	    inicio=0
+	  else:
+	    print("\n>>>ERROR DE MYSQL (conexión o parámetros):")
+	    print("\t>>No es posible conectar MySQL server")
+	    print("\t>>error:",err)
+	    inicio=0
+    
+	else:
+	  while inicio==2:
+	  	print("\n¿Desea realizar otra operación?")
+	  	print("\t1. Sí")
+	  	print("\t2. No")
+	  	operacion=input("Ingrese opción numérica: ")
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `registros`
---
-ALTER TABLE `registros`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `registros`
---
-ALTER TABLE `registros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+	  	if operacion.isnumeric()==True:
+	  		if operacion=="1":
+	  			inicio=0
+	  		elif operacion=="2":
+	  			#cerrando cursor
+	  			print("\n>>Cerrando el cursor...")
+	  			cursor.close()
+	  			#cerrando conexión
+	  			print(">>Desconectando de la DATABASE",database)
+	  			conexion_db.close()
+	  			break
+	  		else:
+	  			print("\n\terror: opción incorrecta")
+	  			inicio=2
+	  	else:
+	  		print("\n\t>>error: debe ingresar un número")
+	  		incio=2
