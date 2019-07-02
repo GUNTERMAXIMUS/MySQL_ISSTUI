ea07c9cfe3a54c2f9e5dfe0471013528a49fda3e
Benny Serra <benjaserrau@gmail.com>
2019-07-01 23:57:40 -0400

    Update bsentor.py

--- bsentor.py
+++ bsentor.py
@@ -18,20 +18,36 @@ port=""
 charset=""
 #dictionary vacío
 configuracion={}
-
-#funciones
-def recorrido_rows():
-	"""Usa fetch-all en una variable para recorrer todas las rows y luego imprimirlas"""
-	rows=cursor.fetchall()#(presenta todo el resultado | FETCH ALL /fetchone presentar la primera row)	
-	print("\n\t>>REGISTROS DATABASE:")
-	for i in rows:
-		print("\t",i)
-def recorrido_rows_indice():
+#funcion/es
+def recorrido_rows_indices(y=False):
 	"""Usa fetch-all en una variable para recorrer todas las rows y luego imprimirlas por índice"""
-	rows=cursor.fetchall()#(presenta todo el resultado | FETCH ALL /fetchone presentar la primera row)	
-	print("\n\t>>REGISTROS DATABASE:")
-	for i in rows:
-		print("\t",i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7])
+	while y==True:
+		y=False
+		print("\n\tSeleccione tipo de recorrido del registro")
+		print("\t1. Sin índice")
+		print("\t2. Con índice")
+		tipo_index=input("\tIngrese opción numérica: ")
+		rows=cursor.fetchall()#(presenta todo el resultado | FETCH ALL /fetchone presentar la primera row)	
+		print("\n\t>>REGISTROS DATABASE:")
+		if tipo_index.isnumeric()==True:
+			if tipo_index=="1":
+				for i in rows:
+					print("\t",i)
+			elif tipo_index=="2":
+				for i in rows:
+					print("\t",i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7])
+			else:
+				error_op(1)
+				y=True
+		else:
+			error_op(2)
+			y=True
+
+def error_op(x=None):
+	if x==1:
+		print("\n\t>error: opción incorrecta")
+	elif x==2:
+	  	print("\n\t>error: debe ingresar un número")
 """------------------------------------------------------------------------------------------------------"""
 while inicio==0:
 	inicio+=1
@@ -62,10 +78,10 @@ while inicio==0:
 			port="3306"
 			charset="utf8"
 		else:
-			print("\n\tdebe ingresar una opción correcta")
+			error_op(1)
 			inicio=0
 	else:
-		print("\n\tdebe ingresar un número")
+		error_op(2)
 		inicio=0
 	
 	#los argumentos de conexión definidos en un diccionario [con keys:values] para su mejor manipulación
@@ -91,8 +107,8 @@ while inicio==0:
 			#MENU PRINCIPAL
 			print("\n\tSELECCIONE UNA OPCIÓN para",database)
 			print("\t1. Extraccion de datos 'registros' en sql")
-			print("\t2. Importar datos CSV")
-			print("\t3. Importar o extrer datos JSON")
+			print("\t2. CSV: importar o extraer datos ")
+			print("\t3. JSON: importar o extraer datos")
 			print("\t4. Exit")
 			main_menu=input("\t>Ingrese una opción numérica: ")
 			if main_menu.isnumeric()==True:
@@ -112,36 +128,36 @@ while inicio==0:
 							if op_sql=="1":								
 								#ejecutar la query
 								cursor.execute("SELECT * FROM registros")
-								recorrido_rows()
+								recorrido_rows_indices(True)
 							elif op_sql=="2":
 								print("\n\tIngrese ID's BETWEEN (puede seleccionar el mismo número para una ID)")
 								id1=input("\t> ID 1: ")
 								id2=input("\t> ID 2: ")
 								cursor.execute("SELECT * FROM registros WHERE id BETWEEN %s AND %s",(id1,id2,))
-								recorrido_rows_indice()
+								recorrido_rows_indices(True)
 							elif op_sql=="3":
 								clave=input("\n\t> clave: ")
 								cursor.execute("SELECT * FROM registros WHERE clave_profesor=%s",(clave,))
-								recorrido_rows_indice()		
+								recorrido_rows_indices(True)		
 							elif op_sql=="4":
 								print("\n\tPara ingresar acentos o caracteres en español, es necesario que su database esté con UTF-8")
 								print("\tDe lo contrario no se imprimirán los resultados buscados")
 								nombre=input("\t\n> nombre: ")
 								apellido=input("\t> apellido: ")
 								cursor.execute("SELECT * FROM registros WHERE nombre=%s AND apellido=%s",(nombre,apellido,))
-								recorrido_rows_indice()							
+								recorrido_rows_indices(True)							
 							elif op_sql=="5":
 								print("Ingrese en el siguiente formato: AAAA/MM/DD")
 								fecha=input("\t>Fecha: ")
 								cursor.execute("SELECT * FROM registros WHERE fecha=%s",(fecha,))
-								recorrido_rows_indice()		
+								recorrido_rows_indices(True)		
 							elif op_sql=="6":
 								inicio=1
 							else:
-								print("\n\terror: opción inválida")
+								error_op(1)
 								x=0
 						else:
-							print("\n\terror: debe ingresar un número")
+							error_op(2)
 							x=0
 
 				elif main_menu=="2":
@@ -172,7 +188,71 @@ while inicio==0:
 					else:
 						print("\n\t>>El archivo no existe")
 						inicio=1
-#				elif main_menu=="3":
+				
+				elif main_menu=="3":
+					#Importar datos Json
+					z=True
+					while z==True:
+						z=False
+						print("\n\tSeleccione una opción de datos")
+						print("\t1. Insertar")
+						print("\t2. Extraer")
+						op_json=input("\tIngrese opción numérica: ")
+						if op_json.isnumeric()==True:
+							if op_json=="1":
+								"""
+								add_reg={}
+								ID="id"
+								CV_P="clave_profesor"
+								NAME="nombre"
+								LAST_N="apellido"
+								DATE="fecha"
+								T_IN="hora_de_entrada"
+								T_OUT="hora_de_salida"
+								ROOM="sala"
+								print("Ingresar datos")
+								id_=input("id_: ")
+								clave_p=input("clave_p: ")
+								name_=input("name_: ")
+								last_n=input("last_n: ")
+								date_=input("date_")
+								t_in=input("t_in: ")
+								t_out=input("t_out: ")
+								room=input("room: ")
+								add_reg[ID]=id_
+								add_reg[CV_P]=clave_p
+								add_reg[NAME]=name_
+								add_reg[LAST_N]=last_n
+								add_reg[DATE]=date_
+								add_reg[T_IN]=t_in
+								add_reg[T_OUT]=t_out
+								add_reg[ROOM]=room
+								
+								add_reg={
+									"id":"12",
+									"clave_profesor":"1234",
+									"nombre":"Vergamin",
+									"apellido":"Perra",
+									"fecha":"2019/12/21",
+									"hora_de_ingreso":"09:34:12",
+									"hora_de_salida":"13:23:03",
+									"sala":"203"
+									}
+								print(add_reg)
+								query_1=("INSERT INTO registros (id,clave_profesor, nombre, apellido, fecha, hora_de_ingreso, hora_de_salida, sala) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)")
+								values_1=("id","clave_profesor","nombre","apellido","fecha","hora_de_ingreso","hora_de_salida","sala")
+								cursor.execute(query_1, values_1, (json.dumps(add_reg),))
+								print("insertados")
+							elif op_json=="2":
+								print(None)
+							else:
+								error_op(1)
+								z=True
+						else:
+							error_op(2)
+							z=True
+							"""
+
 #				elif main_menu=="4":
 
 	#para error de conexión
@@ -209,10 +289,10 @@ while inicio==0:
 	  					elif menus=="2":
 	  						inicio=2
 	  					else:
-	  						print("\n\terror: opcion incorrecta")
+	  						error_op(1)
 	  						inicio=3
 	  				else:
-	  					print("\n\terror: debe ingresar un número")
+	  					error_op(2)
 	  					inicio=3
 	  			
 	  		elif operacion=="2" or operacion=="exit":
@@ -225,8 +305,8 @@ while inicio==0:
 	  			print(">>Desconectado de MySQL.")
 	  			break
 	  		else:
-	  			print("\n\terror: opción incorrecta")
+	  			error_op(1)
 	  			inicio=2
 	  	else:
-	  		print("\n\t>>error: debe ingresar un número")
+	  		error_op(2)
 	  		incio=2
